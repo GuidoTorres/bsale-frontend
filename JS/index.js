23873364e2a8af11feac8a6934ded2fc72104b3d
GuidoTorres <hectortorresdurand@gmail.com>
2021-12-12 18:10:14 -0500

    filtros y categorias

--- JS/index.js
+++ JS/index.js
@@ -1,7 +1,12 @@
 let urlImg = "http://localhost:3000/api/v1/products";
 let urlCategory = "http://localhost:3000/api/v1/categories";
 
-// get all products
+const category = document.querySelector(".select-category");
+const search = document.querySelector(".search");
+const productSection = document.querySelector(".products-section");
+const orderBy = document.querySelector(".select-order");
+
+// Para obtener los productos desde la api
 
 const getProducts = async () => {
   try {
@@ -9,30 +14,30 @@ const getProducts = async () => {
     const data = await response.json();
 
     if (data) {
-      renderData(data);
+      renderAllData(data);
     }
   } catch (error) {
     console.log(error);
   }
 };
+
+// Para obtener las categorias desde la api
 const getCategory = async () => {
   try {
     const response = await fetch(urlCategory);
     const data = await response.json();
-    if (data) {
-      renderCategories(data);
-    } else {
-      console.log("no hay data");
-    }
   } catch (error) {
     console.log(error);
   }
 };
 
-const renderData = (data, info) => {
+// Para renderizar la imagen, nombre, precio, y descuento
+const renderAllData = (data) => {
   const productSection = document.querySelector(".products-section");
 
-  //   productSection.parentNode.removeChild(productSection);
+  //  map a la data del api para insertar los productos en html
+  productSection.innerHTML = "";
+
   data.map((item, i) => {
     productSection.innerHTML += `
     <div>
@@ -40,6 +45,7 @@ const renderData = (data, info) => {
     <label>${item.name}</label>
     <span>
     <p>$ ${item.price}</p>
+    
     ${
       parseInt(item.discount) !== 0 ? `<p>Descuento: ${item.discount}%</p>` : ""
     }
@@ -48,50 +54,78 @@ const renderData = (data, info) => {
   });
 };
 
-const renderCategories = (category) => {
-  console.log(category);
-  const selectContainer = document.querySelector(".select-wrapper");
-  const select = selectContainer.querySelector("select");
-  console.log(selectContainer);
-  console.log(select);
+// Listener para la seleccion de categorias
+category.addEventListener("change", async (e) => {
+  try {
+    const response = await fetch(
+      `http://localhost:3000/api/v1/products/${e.target.value}`
+    );
+    const data = await response.json();
 
-  category.length > 0 &&
-    category.map((item, i) => {
-      select.innerHTML += `<option value="${item.id}">${item.name}</option>`;
-    });
-};
-
-const search = document.querySelector(".search");
-
-search.addEventListener("keyup", (e) => {
-  var request = new XMLHttpRequest();
-
-  // Instantiating the request object
-  request.onload = function () {
-    e.preventDefault();
-
-    renderData(request.response, "search");
-    console.log(request.response);
-  };
-  request.open(
-    "GET",
-    `http://localhost:3000/api/v1/products/search?term=${e.target.value}`
-  );
-
-  request.responseType = "json";
-  request.send();
+    if (data) {
+      renderAllData(data);
+    }
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-getProducts();
-getCategory();
+// Listener para el buscador
+search.addEventListener("keyup", async (e) => {
+  //peticion usando ajax
+
+  try {
+    const response = await fetch(
+      `http://localhost:3000/api/v1/products/search?term=${e.target.value}`
+    );
+    const data = await response.json();
+
+    if (data) {
+      renderAllData(data);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+//Listener para los filtros
+orderBy.addEventListener("change", async (e) => {
+  try {
+    const response = await fetch(
+      `http://localhost:3000/api/v1/products?${
+        e.target.value === "1"
+          ? "desc"
+          : e.target.value === "2"
+          ? "asc"
+          : e.target.value === "3"
+          ? "disc"
+          : ""
+      }=${e.target.value}`
+    );
+    console.log(response);
+    const data = await response.json();
+
+    if (data) {
+      renderAllData(data);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// Listener para el select e input de materialize de materialize
 document.addEventListener("DOMContentLoaded", function () {
   var elems = document.querySelectorAll(".dropdown-trigger");
+  var sel = document.querySelectorAll("select");
   var instances = M.Dropdown.init(elems, {
     coverTrigger: false,
     constrainWidth: false,
   });
+  var instances = M.FormSelect.init(sel, {
+    constrainWidth: false,
+    coverTrigger: false,
+  });
 });
-document.addEventListener("DOMContentLoaded", function () {
-  var sel = document.querySelectorAll("select");
-  var instances = M.FormSelect.init(sel, {});
-});
+
+getProducts();
+getCategory();
