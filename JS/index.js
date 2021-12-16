b7732f3fb947e8e1b2eb0311bd5438ed2b1b4ea2
GuidoTorres <hectortorresdurand@gmail.com>
2021-12-16 10:14:17 -0500

    correciones de ultimo minuto

--- JS/index.js
+++ JS/index.js
@@ -62,6 +62,7 @@ const renderAllData = (data) => {
   });
 };
 
+//Para renderizar las categorias en el select
 const renderCategories = (data) => {
   // inserto la primera opcion al select
 
@@ -72,44 +73,67 @@ const renderCategories = (data) => {
   });
 };
 
-// event listener para poder usar el filtro de categoria y de ordenar juntos
-// uso un map para poder usar los dos a la vez dependiendo si hay en uno u otro un cabio se realiza el fethc
 [category, orderBy].map((element, i) =>
   element.addEventListener("change", async (e) => {
+    const orderBy = document.querySelector("#orderBy");
+
     const cat = category.value;
-    const ord = orderBy.value;
+    const orderValue = orderBy.value;
+    console.log(orderValue);
 
     //dependiendo del valor de category u orderBy paso los parametros, si solo hay categoria lo demas no se envia
     //si paso ambos se junta la busqueda y el filtro de ordenar por precio
-    try {
-      const response = await fetch(
-        `https://bsale-test1.herokuapp.com/api/v1/products/${cat}?${
-          ord === "d" ? "desc" : ord === "a" ? "asc" : ord === "s" ? "disc" : ""
-        }=${ord}`
-      );
-      const data = await response.json();
-      // una vez obtenida la data del fetch la paso a la funcion renderAll data que coloca las imagens y nombres en el html
-      if (data) {
-        renderAllData(data);
+    if (cat !== "Seleccione categoria") {
+      try {
+        console.log("categoria + order");
+        const response = await fetch(
+          `https://bsale-test1.herokuapp.com/api/v1/products/${cat}?${
+            orderValue === "d"
+              ? "desc"
+              : orderValue === "a"
+              ? "asc"
+              : orderValue === "s"
+              ? "disc"
+              : ""
+          }=${orderValue}`
+        );
+        const data = await response.json();
+        // una vez obtenida la data del fetch la paso a la funcion renderAll data que coloca las imagens y nombres en el html
+        if (data) {
+          renderAllData(data);
+        }
+      } catch (error) {
+        console.log(error);
       }
-    } catch (error) {
-      console.log(error);
     }
   })
 );
 
+//Aca combino los la busqueda y el filtro por precio
+const SearchFunction = async (e) => {
+  const search = document.querySelector(".search");
+  const orderBy = document.querySelector("#orderBy");
+  const category = document.querySelector(".select-category");
 
-// Event listener del buscador no pude hacerlo funcionar con los otros filtros 
-search.addEventListener("keyup", async (e) => {
-  const ord = menu.innerText;
+  // const orderVAlue = e.target.innerHTML;
+  const cat = category.value;
+  const orderValue = orderBy.value;
+  const searchValue = search.value;
+  const menuValue = e.target.innerText;
+  console.log(searchValue);
 
-  if (events === "Enter") {
+  // aca valido que se ejecute la busqueda solamente si se presiona enter
+  if (e.code === "Enter" && searchValue !== "") {
+    //almaceno el termino de busqueda en el localstorage para poder usarlo despues en una busqueda combinada con los filtros
     try {
+      console.log("busqueda enter");
+      //aca realizo la busqueda del termino usando el termino almacenado en el localstorage
       const response = await fetch(
-        `https://bsale-test1.herokuapp.com/api/v1/products/search?term=${e.target.value}`
+        `https://bsale-test1.herokuapp.com/api/v1/products/search?term=${searchValue}`
       );
       const data = await response.json();
 
+      // si hay respuesta con la data se renderiza todo
       if (data) {
         renderAllData(data);
       }
@@ -117,37 +141,70 @@ search.addEventListener("keyup", async (e) => {
       console.log(error);
     }
   }
-});
 
-
-//event listener de los filtros en resposive no lo pude hacer funcionar en combinado con las categorias y el buscador
-//debido a que es otro tipo de evento, en cambio en la version web los dos son select y use event change
-menu.addEventListener("click", async (e) => {
-  const ord = e.target.innerHTML;
-
-  try {
-    const response = await fetch(
-      `https://bsale-test1.herokuapp.com/api/v1/products?${
-        ord === "Precio (mayor a menor)"
-          ? "desc"
-          : ord === "Precio (menor a mayor)"
-          ? "asc"
-          : ord === "Descuento (menor a mayor)"
-          ? "disc"
-          : ""
-      }=${ord}`
-    );
-    const data = await response.json();
-
-    if (data) {
-      renderAllData(data);
+  // este if se cuando hay una busqueda y se selecion un filtro
+  if ((searchValue !== "" && orderValue) || (menuValue && cat === "")) {
+    try {
+      console.log("busqueda y order");
+      //aca si hay un termino de busqueda en el localstorage hace la busqueda, pero ademas se le agrega otros parametros
+      //que serian si se ordena ascdentemente o las otras opciones para asi combinar ambos
+      const response =
+        orderValue !== ""
+          ? await fetch(
+              `https://bsale-test1.herokuapp.com/api/v1/products/search?term=${searchValue}&${
+                orderValue === "d"
+                  ? "desc"
+                  : orderValue === "a"
+                  ? "asc"
+                  : orderValue === "s"
+                  ? "disc"
+                  : ""
+              }=${
+                orderValue === "d"
+                  ? 1
+                  : orderValue === "a"
+                  ? 2
+                  : orderValue === "s"
+                  ? 3
+                  : ""
+              }`
+            )
+          : await fetch(
+              `https://bsale-test1.herokuapp.com/api/v1/products/search?term=${searchValue}&${
+                menuValue === "Precio (mayor a menor)"
+                  ? "desc"
+                  : menuValue === "Precio (menor a mayor)"
+                  ? "asc"
+                  : menuValue === "Descuento (menor a mayor)"
+                  ? "disc"
+                  : ""
+              }=${
+                menuValue === "Precio (mayor a menor)"
+                  ? 1
+                  : menuValue === "Precio (menor a mayor)"
+                  ? 2
+                  : menuValue === "Descuento (menor a mayor)"
+                  ? 3
+                  : ""
+              }`
+            );
+      const data = await response.json();
+      if (data) {
+        renderAllData(data);
+      }
+    } catch (error) {
+      console.log(error);
     }
-  } catch (error) {
-    console.log(error);
   }
-});
+};
 
-//Event listener para el buscador
+//event listener relacionado a la busqueda
+search.addEventListener("keyup", SearchFunction, false);
+
+//event listener relacionado a los filtros en responsive
+menu.addEventListener("click", SearchFunction, false);
+
+orderBy.addEventListener("change", SearchFunction, false);
 
 getProducts();
 getCategory();
