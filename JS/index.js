038b8e23d4c82437cb76a7a52bcdd824f4385d71
Hector <gt12930@GMAIL.com>
2021-12-20 22:33:20 -0500

    correciones

--- JS/index.js
+++ JS/index.js
@@ -74,24 +74,24 @@ const renderCategories = (data) => {
 };
 
 //En esta funcion combino la busqueda y el filtro por precio
-const SearchFunction = async (e) => {
+const searchProducts = async (e) => {
   const search = document.querySelector(".search");
-  const orderBy = document.querySelector("#orderBy");
   const category = document.querySelector(".select-category");
 
-  const cat = category.value;
-  const orderValue = orderBy.value;
   const searchValue = search.value;
-  const menuValue = e.target.innerText;
+
+  if (searchValue !== "" || (null && e.code === "Enter")) {
+    category.selectedIndex = 0;
+  }
 
   // aca valido que se ejecute la busqueda solamente si se presiona enter
   if (e.code === "Enter" && searchValue !== "") {
+    console.log("busqueda con enter");
     try {
       const response = await fetch(
         `https://bsale-test1.herokuapp.com/api/v1/products/search?term=${searchValue}`
       );
       const data = await response.json();
-
       // si hay respuesta con la data se renderiza todo
       if (data) {
         renderAllData(data);
@@ -102,66 +102,10 @@ const SearchFunction = async (e) => {
   }
 
   // este if se cuando hay una busqueda y se selecciona un filtro
-  if ((searchValue !== "" && orderValue) || (menuValue && cat === "")) {
-    try {
-      console.log("busqueda y order");
-      const response =
-        //esta funcion se da cuando hay una busqueda y un filtro seleccionado
-
-        orderValue !== ""
-          ? await fetch(
-              `https://bsale-test1.herokuapp.com/api/v1/products/search?term=${searchValue}&${
-                //dependiendo del valor de order value se cambia el parametro para cambiar el filtro a ascendente descendente
-                orderValue === "d"
-                  ? "desc"
-                  : orderValue === "a"
-                  ? "asc"
-                  : orderValue === "s"
-                  ? "disc"
-                  : ""
-              }=${
-                orderValue === "d"
-                  ? 1
-                  : orderValue === "a"
-                  ? 2
-                  : orderValue === "s"
-                  ? 3
-                  : ""
-              }`
-            )
-          : //esta funcion se da cuando hay una busqueda y un filtro del menu en resposive
-
-            await fetch(
-              `https://bsale-test1.herokuapp.com/api/v1/products/search?term=${searchValue}&${
-                menuValue === "Precio (mayor a menor)"
-                  ? "desc"
-                  : menuValue === "Precio (menor a mayor)"
-                  ? "asc"
-                  : menuValue === "Descuento (menor a mayor)"
-                  ? "disc"
-                  : ""
-              }=${
-                menuValue === "Precio (mayor a menor)"
-                  ? 1
-                  : menuValue === "Precio (menor a mayor)"
-                  ? 2
-                  : menuValue === "Descuento (menor a mayor)"
-                  ? 3
-                  : ""
-              }`
-            );
-      const data = await response.json();
-      if (data) {
-        renderAllData(data);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  }
 };
 
 //funcion que junta el filtro de categoria con el de precios
-const orderFunction = async (e) => {
+const categoriesAndFilters = async (e) => {
   const category = document.querySelector(".select-category");
   const orderBy = document.querySelector("#orderBy");
 
@@ -169,8 +113,12 @@ const orderFunction = async (e) => {
   const cat = category.value;
   const menuValue = e.target.innerHTML;
 
-  // si se seleccion una categoria se hace el try
+  if (cat > 0) {
+    search.value = "";
+  }
+
   if (cat !== "Seleccione categoria") {
+    console.log("categories and filtre");
     try {
       //si se obtiene un valor desde el select se hace este fetch obteniendo categoria y orden
       const response =
@@ -218,16 +166,167 @@ const orderFunction = async (e) => {
   }
 };
 
-//event listener relacionado a la busqueda
-search.addEventListener("keyup", SearchFunction, false);
-menu.addEventListener("click", SearchFunction, false);
-orderBy.addEventListener("change", SearchFunction, false);
+const searchAndFilters = async (e) => {
+  const orderBy = document.querySelector("#orderBy");
+  const search = document.querySelector(".search");
 
-//event listener que relaciona los filtros
-category.addEventListener("change", orderFunction, false);
-orderBy.addEventListener("change", orderFunction, false);
-menu.addEventListener("click", orderFunction, false);
-search.addEventListener("keyup", orderFunction, false);
+  const searchValue = search.value;
+  const orderValue = orderBy.value;
+  const menuValue = e.target.innerHTML;
+
+  if (searchValue !== "") {
+    orderBy.selectedIndex = 0;
+  }
+
+  if (searchValue !== "" && orderValue !== "") {
+    try {
+      console.log("search and filtre");
+      const response = await fetch(
+        `https://bsale-test1.herokuapp.com/api/v1/products/search?term=${searchValue}&${
+          orderValue === "d"
+            ? "desc"
+            : orderValue === "a"
+            ? "asc"
+            : orderValue === "s"
+            ? "disc"
+            : ""
+        }=${orderValue}`
+      );
+      //esta funcion se da cuando hay una busqueda y un filtro del menu en resposive
+
+      const data = await response.json();
+      if (data) {
+        renderAllData(data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  if (searchValue !== "" && menuValue !== "" && orderValue === "") {
+    console.log("search and filtre");
+
+    try {
+      const response = await fetch(
+        `https://bsale-test1.herokuapp.com/api/v1/products/search?term=${searchValue}&${
+          menuValue === "Precio (mayor a menor)"
+            ? "desc"
+            : menuValue === "Precio (menor a mayor)"
+            ? "asc"
+            : menuValue === "Descuento (menor a mayor)"
+            ? "disc"
+            : ""
+        }=${
+          menuValue === "Precio (mayor a menor)"
+            ? 1
+            : menuValue === "Precio (menor a mayor)"
+            ? 2
+            : menuValue === "Descuento (menor a mayor)"
+            ? 3
+            : ""
+        }`
+      );
+      const data = await response.json();
+      if (data) {
+        renderAllData(data);
+      }
+    } catch (error) {}
+  }
+};
+
+// funcion para solo ordenar productos por precio
+const orderProducts = async (e) => {
+  const orderBy = document.querySelector("#orderBy");
+  const category = document.querySelector(".select-category");
+  const search = document.querySelector(".search");
+
+  const searchValue = search.value;
+  const cat = category.value;
+  const orderValue = orderBy.value;
+  const menuValue = e.target.innerHTML;
+
+  if (
+    orderValue !== "" &&
+    cat === "Seleccione categoria" &&
+    searchValue === ""
+  ) {
+    try {
+      const response = await fetch(
+        `https://bsale-test1.herokuapp.com/api/v1/products?${
+          orderValue === "d"
+            ? "desc"
+            : orderValue === "a"
+            ? "asc"
+            : orderValue === "s"
+            ? "disc"
+            : ""
+        }=${orderValue}`
+      );
+      //esta funcion se da cuando hay una busqueda y un filtro del menu en resposive
+
+      const data = await response.json();
+      if (data) {
+        renderAllData(data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  if (
+    menuValue !== "" &&
+    cat === "Seleccione categoria" &&
+    searchValue === "" &&
+    orderValue === ""
+  ) {
+    try {
+      const response = await fetch(
+        `https://bsale-test1.herokuapp.com/api/v1/products?${
+          menuValue === "Precio (mayor a menor)"
+            ? "desc"
+            : menuValue === "Precio (menor a mayor)"
+            ? "asc"
+            : menuValue === "Descuento (menor a mayor)"
+            ? "disc"
+            : ""
+        }=${
+          menuValue === "Precio (mayor a menor)"
+            ? 1
+            : menuValue === "Precio (menor a mayor)"
+            ? 2
+            : menuValue === "Descuento (menor a mayor)"
+            ? 3
+            : ""
+        }`
+      );
+      //esta funcion se da cuando hay una busqueda y un filtro del menu en resposive
+
+      const data = await response.json();
+      if (data) {
+        renderAllData(data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+};
+
+//event listener relacionado a la busqueda
+search.addEventListener("keyup", searchProducts, false);
+
+//event listener que relaciona los filtros y categorias
+category.addEventListener("change", categoriesAndFilters, false);
+orderBy.addEventListener("change", categoriesAndFilters, false);
+menu.addEventListener("click", categoriesAndFilters, false);
+
+//event listener que relaciona la busqueda con los filtros
+search.addEventListener("keyup", searchAndFilters, false);
+menu.addEventListener("click", searchAndFilters, false);
+orderBy.addEventListener("change", searchAndFilters, false);
+
+//solo para ordenar todos los productos
+menu.addEventListener("click", orderProducts, false);
+orderBy.addEventListener("change", orderProducts, false);
 
 getProducts();
 getCategory();
